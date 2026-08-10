9ee9464cf603e1ca1e2a6e5014a3d08ad4b818cb
Danila Sukharev <dan.sukharev@gmail.com>
2026-08-10 22:27:26 +0300

    osd: add rootless UDP network transport

--- src/osd/modules/lib/osdobj_common.cpp
+++ src/osd/modules/lib/osdobj_common.cpp
@@ -288,6 +288,7 @@ void osd_common_t::register_options()
 
 	REGISTER_MODULE(m_mod_man, NETDEV_TAPTUN);
 	REGISTER_MODULE(m_mod_man, NETDEV_PCAP);
+	REGISTER_MODULE(m_mod_man, NETDEV_UDP);
 	REGISTER_MODULE(m_mod_man, NETDEV_NONE);
 
 #ifndef NO_USE_MIDI
